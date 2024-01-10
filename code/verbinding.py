79fa9a09c23687d66c56891f7120f4f483140be8
Timo Nijkamp <timonijkamp@gmail.com>
2024-01-10 22:50:57 +0100

    removed some files

--- code/verbinding.py
+++
@@ -1,9 +0,0 @@
-class Verbinding: 
-    def __init__(self, station_1, station_2, duur):
-        self.station_1 = station_1
-        self.station_2 = station_2
-        self.duur = duur
-        self.isgelegd = False
-
-    def toggle_isgelegd(self):
-        self.isgelegd = True 
