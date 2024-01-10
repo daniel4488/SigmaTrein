c99b395c593022657ff3cf2f1fbbedd7355267d6
Timo Nijkamp <timonijkamp@gmail.com>
2024-01-11 00:21:15 +0100

    added __init__.py -> project is now a module and relative imports work

---
+++ code/algorithms/randomize.py
@@ -0,0 +1,11 @@
+from code.classes.traject import Traject
+
+class Randomize:
+    def __init__(self):
+        randomized_trajectory = []
+
+    def make_random_trajectory():
+        pass
+
+    
+    
