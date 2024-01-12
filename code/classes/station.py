162f8d7e8149f602824ea7c92af2ef67c05ab1d1
Daniel Frijns <daniel.frijns2@student.uva.nl>
2024-01-12 22:52:01 +0100

    station class: added docstrings and translated some dutch leftovers

--- code/classes/station.py
+++ code/classes/station.py
@@ -1,23 +1,31 @@
 class Station:
-    """ Class voor station. """
+    """ Class representing a train station. """
 
     def __init__(self, station_name: str, x: float, y: float):
+        """ Creates a new Station object. """
         self.station_name: str = station_name
         self.x: float = x
         self.y: float = y
         self.connections: list[int] = []
         self.possible_connections: list[int] = []
 
-    def add_connection(self, traject_nummer: int) -> None:
-        """ Voeg nieuwe verbinding toe aan het station. """
-        self.connections.append(traject_nummer)
+    def add_connection(self, connection_number: int) -> None:
+        """ Adds a new connection to the train station. """
+        self.connections.append(connection_number)
     
     def repopulate_possible_connections(self) -> None:
+        """ Add connections to possible connections. """
+        # Possible bug: could be that there are more possible connections than connections
+        # In that case there will be duplicates in possible connections
+        # Question: is order important for connections? if not -> change to set
+        # A set will also prevent having duplicates
         for value in self.connections:
             self.possible_connections.append(value)
 
-    def remove_possible_connection(self, verbinding_nummer: int) -> None:
-        self.possible_connections.remove(verbinding_nummer)
+    def remove_possible_connection(self, connection_number: int) -> None:
+        """ Remove a possible connection from the list. """
+        self.possible_connections.remove(connection_number)
 
     def __str__(self) -> str:
+        """ Gives the string representation. """
         return self.station_name
