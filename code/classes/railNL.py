53419951afe11c5daacd0e4a744d041da2e6afc9
Daniel Frijns <daniel.frijns2@student.uva.nl>
2024-01-13 14:01:49 +0100

    railNl.py: docstrings and translation to english

--- code/classes/railNL.py
+++ code/classes/railNL.py
@@ -1,23 +1,28 @@
 from code.classes.station import Station
-from code.classes.traject import Traject
 from code.classes.connection import Connection
 
 
 class RailNL:
+    """ The RailNL class loads the csv data into memory. """
+
     def __init__(self) -> None:
-        # Dict met de naam van het station als key en een bijbehordend object
+        """ Creates a RailNL object. """
+
+        # Dictionary with station name as key mapping to its object
         self.stations: dict[str, Station] = {}
-        # Dict met het trajectnummer als key en een bijbehordend object
-        self.verbindingen: dict[int, Connection] = {}
-        # Dict met de trajectnaam als key en een bijbehordend object
-        self.trajecten: list[Traject] = []
 
-        # Laad de stations in
-        self.laad_stations()
-        # Laad de verbindingen in
-        self.laad_verbindingen()
+        # Dictionary with trajectory number as key mapping to its object
+        self.connections: dict[int, Connection] = {}
 
-    def laad_stations(self) -> None:
+        # Loads stations
+        self.load_stations()
+
+        # Loads connections
+        self.load_connections()
+
+    def load_stations(self) -> None:
+        """ Converts the station data to Station classes. """
+        
         # Open StationsHolland.csv
         with open("data/holland/StationsHolland.csv", "r") as file:
             # Remove header
@@ -31,35 +36,36 @@ class RailNL:
                 # Split line based on a comma
                 station, y, x = line.split(",")
                 
+                # Create station object
                 self.stations[station] = Station(station, float(x), float(y))
 
-    def laad_verbindingen(self) -> None:
+    def load_connections(self) -> None:
+        """ Converts connections data to Connection classes. """
 
         with open("data/holland/ConnectiesHolland.csv", "r") as file:
             # Remove header
             _ = file.readline()
 
-            traject_nummer = 0
+            # Keep track of the current trajectory number
+            trajectory_number = 0
 
             for line in file:
                 # Remove newline character
                 line = line.strip()
 
                 # Split line
-                station_1, station_2, duur_str = line.split(",")
+                station_1, station_2, duration_str = line.split(",")
 
-                # Convert van string naar integer
-                duur = int(duur_str)
+                # Convert string to integer
+                duration = int(duration_str)
 
-                # Voeg verbinding toe voor beide stations
-                self.stations[station_1].add_connection(traject_nummer)
-                self.stations[station_2].add_connection(traject_nummer)
+                # Add connection to both stations
+                self.stations[station_1].add_connection(trajectory_number)
+                self.stations[station_2].add_connection(trajectory_number)
 
-                # Voeg verbinding toe 
-                self.verbindingen[traject_nummer] = (Connection(traject_nummer, station_1, station_2, duur))
+                # Add a connection
+                self.connections[trajectory_number] = (Connection(trajectory_number, station_1, station_2, duration))
 
-                # Houd bij welk trajectnummer dit was
-                traject_nummer += 1
+                # Increment the current trajectory number
+                trajectory_number += 1
                 
-            
-
