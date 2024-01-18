b4fc3cc3ac500ed0f5ba1de2b17321c69be37878
Daniel Frijns <daniel.frijns2@student.uva.nl>
2024-01-18 16:22:08 +0100

    added command line argument to main using argparse

--- main.py
+++ main.py
@@ -3,10 +3,31 @@ from code.algorithms.randomize_trajectory import RandomizeTrajectory
 from code.visualisation.plot_single_track import PlotlyLoad
 from code.visualisation.baseline import visualize_baseline, visualize_iterations_to_score
 
+import argparse
+
+
+def valid_argument(arg: str) -> str:
+    """ Function to check if the given command line argument is correct. """
+    valid_arguments = {"holland", "nationaal"}
+
+    if arg not in valid_arguments:
+        raise argparse.ArgumentTypeError
+    return arg
+
 
 if __name__ == "__main__":
+
+    # initialise parser
+    parser = argparse.ArgumentParser(description="Run the main function of the RailNL case.")
+
+    # add command line argument with default value
+    parser.add_argument("--dataset", "-d", default="holland", type=valid_argument)
+
+    # parse the command line argument
+    args = parser.parse_args()
+
     # Initialize raiLNL
-    railNL = RailNL(dataset="holland")
+    railNL = RailNL(dataset=args.dataset)
 
     randomize = RandomizeTrajectory(railNL.stations, railNL.connections)
 
