dc634a2eb8a18adef8d028cfdc0ba0357d189a17
Roos1309 <roos.van.der.neut@student.uva.nl>
2024-01-19 17:56:34 +0100

    abstractions in randomize and layout changes

--- code/algorithms/randomize_trajectory.py
+++ code/algorithms/randomize_trajectory.py
@@ -15,7 +15,7 @@ class RandomizeTrajectory:
     def __init__(self, stations: dict[str, Station], connections: dict[int, Connection]) -> None:
         """ Initiates the random algorithm. """
 
-        # Make algorithm pseudo random
+        # make algorithm pseudo random
         random.seed(324488)
 
         self.stations: dict[str, Station] = stations
@@ -24,65 +24,92 @@ class RandomizeTrajectory:
         self.used_connections: set = set()
         self.solution: dict[int, list[str]] = {}
 
+
+    def choose_station(self, stations: list[str]) -> list[str, Station]:
+        """ Chooses a random station from the given list and returns a list
+            with its name and object. """
+
+        name = random.choice(stations)
+        station = self.stations[name]
+        return [name, station]
+
+
+    def get_station(self, start: str, connection: int) -> list[str, Station]:
+        """ Returns list with station name and object from given connection number. """
+
+        name = self.connections[connection].get_destination_station(start)
+        station = self.stations[name]
+        return [name, station]
+
+
     def repopulate_possible_connections_for_all_stations(self) -> None:
         """ Prepare for the generation of a new trajectory. """
+
         for station in self.stations:
             self.stations[station].repopulate_possible_connections()
 
-    def make_random_trajectory(self) -> Trajectory:
+
+    def update_connections(self, connection: int, departure: Station, destination: Station) -> None:
+        """ Removes a made connection from the possible connections of the
+            departure station, as well as from the destination station. """
+
+        departure.remove_possible_connection(connection)
+        destination.remove_possible_connection(connection)
+
+
+    def update_trajectory(self, duration: int, connection: int, station: str, trajectory: Trajectory) -> None:
+        """ Adds new connection and station to the given trajectory, and
+            updates its total time. """
+
+        trajectory.add_station_to_trajectory(station)
+        trajectory.duration = duration
+        trajectory.add_connection_number(connection)
+
+
+    def make_trajectory(self) -> Trajectory:
         """ Generates a randomly chosen trajectory. """
         
         self.repopulate_possible_connections_for_all_stations()
 
         # initialize empty trajectory
-        random_trajectory = Trajectory()
+        trajectory = Trajectory()
         
         # choose a random station to depart from
-        random_departure_station = random.choice(list(self.stations.keys()))
-        random_departure_station_object = self.stations[random_departure_station]
+        departure_station = self.choose_station(list(self.stations.keys()))
 
         # add departure station to the trajectory
-        random_trajectory.add_station_to_trajectory(random_departure_station)
+        trajectory.add_station_to_trajectory(departure_station[0])
 
         # add stations to trajectory as long as its duration is less than 120 mins,
         # and there are still possible connections
-        while random_trajectory.duration <= 120 and random_departure_station_object.possible_connections:
+        while trajectory.duration <= 120 and departure_station[1].possible_connections:
             
             # choose random connection number from possible connections at departure station
-            random_connection = random.choice(random_departure_station_object.possible_connections)
-            
+            connection = random.choice(departure_station[1].possible_connections)
 
-            # get name of destination station from chosen connection 
-            random_destination_station = self.connections[random_connection].get_destination_station(random_departure_station)
-
-            # get destination station object
-            random_destination_station_object = self.stations[random_destination_station]
+            # get destination station from chosen connection 
+            destination_station = self.get_station(departure_station[0], connection)
 
             # remove created connection from destination and departure station's possible connections
-            random_destination_station_object.remove_possible_connection(random_connection)
-            random_departure_station_object.remove_possible_connection(random_connection)
+            self.update_connections(connection, departure_station[1], destination_station[1])
 
             # update total duration of the trajectory
-            duration_candidate = random_trajectory.duration + self.connections[random_connection].duration
+            duration_candidate = trajectory.duration + self.connections[connection].duration
+
             # add station to trajectory if it fits within 120 mins
             if duration_candidate <= 120:
-                # add station to trajectory
-                random_trajectory.add_station_to_trajectory(random_destination_station)
-
-                # promote candidate
-                random_trajectory.duration = duration_candidate
-
-                random_trajectory.add_connection_number(random_connection)
+                self.update_trajectory(duration_candidate, connection, destination_station[0], trajectory)
 
                 # update departure station to the current station
-                random_departure_station = random_destination_station
-                random_departure_station_object = random_destination_station_object
+                departure_station = destination_station
 
         # create trajectory list of stations
-        return random_trajectory
+        return trajectory
+
 
     def reset_used_connections(self) -> None:
         self.used_connections.clear()
+
 
     @staticmethod
     def clear_scores_file() -> None:
@@ -101,27 +128,29 @@ class RandomizeTrajectory:
         with open("data/scores/random.csv", "w") as file:
             file.write("score\n")
 
-    def make_random_solution(self, write_output: bool) -> Solution | Output:
+    def make_solution(self, write_output: bool) -> Solution | Output:
+        """ Creates a solution of max. 7 trajectories. """
+
         self.reset_used_connections()
 
         trajectories = set()
         is_valid = False
 
         for _ in range(7):
-            # Make random trajectory
-            current_trajectory = self.make_random_trajectory()
+            # make random trajectory
+            current_trajectory = self.make_trajectory()
 
-            # Add to set of trajectories
+            # add to set of trajectories
             trajectories.add(current_trajectory)
 
-            # Add trajectory connections to used_connections
+            # add trajectory connections to used_connections
             self.used_connections.update(current_trajectory.connections)
 
             if len(self.used_connections) == RailNL.NUMBER_OF_CONNECTIONS:
                 is_valid = True
                 break
 
-        # Create solution instance
+        # create solution instance
         if write_output:
             solution = Output(trajectories, is_valid)
         else:
@@ -138,7 +167,7 @@ class RandomizeTrajectory:
     def make_baseline(self) -> None:
         self.prepare_csv_file()
 
-        number_of_simulations = 100000
+        number_of_simulations = 100
 
         for _ in range(number_of_simulations):
-            self.make_random_solution(write_output=True)
+            self.make_solution(write_output=True)
