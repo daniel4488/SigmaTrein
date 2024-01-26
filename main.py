26e0267202d823c038776f838842d4c82dd332c7
Daniel Frijns <daniel.frijns2@student.uva.nl>
2024-01-26 21:29:15 +0100

    simplified main.py arg parsing

--- main.py
+++ main.py
@@ -6,34 +6,16 @@ from code.visualisation.baseline import visualize_baseline, visualize_iterations
 import argparse
 
 
-def valid_dataset(arg: str) -> str:
-    """ Function to check if the given dataset argument is correct. """
-    valid_arguments = {"holland", "nationaal"}
-
-    if arg not in valid_arguments:
-        raise argparse.ArgumentTypeError
-    return arg
-
-
-def valid_verbose(arg: str) -> bool:
-    """ Function to check if the given verbose argument is correct. """
-    valid_arguments = {"False", "True"}
-
-    if arg not in valid_arguments:
-        raise argparse.ArgumentTypeError
-    return bool(arg)
-
-
 if __name__ == "__main__":
 
     # initialise parser
     parser = argparse.ArgumentParser(description="Run the main function of the RailNL case.")
 
     # add dataset command line argument with default value
-    parser.add_argument("--dataset", "-d", default="holland", type=valid_dataset)
+    parser.add_argument("--dataset", "-d", default="nationaal", choices=["holland", "nationaal"])
 
     # add verbose command line argument
-    parser.add_argument("--verbose", "-v", default=False, type=valid_verbose)
+    parser.add_argument("--verbose", "-v", default=False, type=bool, nargs="?", const=True)
 
     # parse the command line argument
     args = parser.parse_args()
