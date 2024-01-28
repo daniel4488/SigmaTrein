1689b5828b939cb13cabbb375caa4aa7134a6369
Daniel Frijns <daniel.frijns2@student.uva.nl>
2024-01-28 20:34:53 +0100

    reformat main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from code.classes.railNL import RailNL
-from code.algorithms.randomize import Randomize
+# from code.algorithms.randomize import Randomize
 from code.visualisation.plot_single_track import PlotlyLoad
 from code.visualisation.baseline import visualize_baseline, visualize_iterations_to_score
+from code.functions.to_camel_case import to_camel_case
 
 import argparse
 import textwrap
@@ -61,27 +62,34 @@ if __name__ == "__main__":
     parser.add_argument(
         "--verbose", "-v",
         action="store_true",
+        default=False,
         help="prints detailed debugging statements"
     )
 
     # add algorithm as positional command line argument
-    # parser.add_argument(
-    #     "algorithm",
-    #     choices=["randomize", "less_random", "hill_climber", "simulated_annealing", "genetic", "sigma"],
-    #     help="choose an algorithm to run"
-    # )
+    parser.add_argument(
+        "algorithm",
+        choices=["randomize", "less_random", "hill_climber", "simulated_annealing", "genetic", "sigma"],
+        help="choose an algorithm to run"
+    )
 
     # parse the command line argument
     args = parser.parse_args()
 
-    # initialize raiLNL
-    # railNL = RailNL(dataset=args.dataset)
+    # class name of algorithm
+    algorithm_class = to_camel_case(args.algorithm)
+
+    # import chosen algorithm
+    exec(f"from code.algorithms.{args.algorithm} import {algorithm_class}")
 
     # initialise random algorithm
-    randomize = Randomize(args.dataset)
+    exec(f"{args.algorithm} = {algorithm_class}('{args.dataset}')")
+
+    # run chosen algorithm
+    exec(f"{args.algorithm}.make_baseline(verbose={args.verbose})")
 
     # make baseline
-    randomize.make_baseline(verbose=args.verbose)
+    # randomize.make_baseline(verbose=args.verbose)
 
     # csv
     data = "data/scores/random.csv"
