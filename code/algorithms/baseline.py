06c5b607c29f8307b29d1053fbd95451cdd44541
Timo Nijkamp <timonijkamp@gmail.com>
2024-01-30 07:34:21 +0100

    final adjustments

--- code/algorithms/baseline.py
+++ code/algorithms/baseline.py
@@ -14,6 +14,7 @@ class Baseline(Randomize, MapVisualization):
     def __init__(self, dataset: str) -> None:
 
         super().__init__(dataset=dataset)
+        # Create score file
         self.score_file = ScoreFile("baseline.csv")
         self.highest_score = 0
         self.highest_score_solution: Solution = None
@@ -22,20 +23,27 @@ class Baseline(Randomize, MapVisualization):
         """ Runs baseline for random algorthm. """
 
         print("Creating baseline...")
+
         self.verbose = verbose
 
         self.score_file.prepare_file()
 
+        # set a parametere that keeps track of the amount of iterations
         i = 0
 
         try:
             # create solutions and write score to score file
             for _ in range(iterations):
+                # print iterations
                 if i % 10000 == 0:
                     print(f"{i} iterations")
+                
                 solution = super().run(iterations=1, visualize=False, verbose=verbose, write_output=True, auto_open=auto_open, unique=unique, prefixed=prefixed)
+
+                # write score to csv file
                 self.score_file.write_score(solution.score)
 
+                # check if a new highest score has been found
                 if solution.score > self.highest_score:
                     self.highest_score = solution.score
                     self.highest_score_solution = solution
@@ -48,11 +56,11 @@ class Baseline(Randomize, MapVisualization):
         # create output object
         Output(self.highest_score_solution.trajectories, True)
 
-        # create visualisation if visualize is True
+        # visualize results
         if visualize:
             data = "data/scores/baseline.csv"
-            # map
-            self.visualize(solution=self.highest_score_solution, auto_open=auto_open)
 
+            # create map
+            self.visualize(solution=self.highest_score_solution, auto_open=auto_open)
             # histogram of scores from random algorithm
             visualize_baseline(data)
